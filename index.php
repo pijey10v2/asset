64e3a9cbae9ce6b6adcb6f96b033e5c04d4d3764
pao <paolo.jon@digile.com>
2025-10-20 10:09:57 +0800

    updated api headers

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Origin: *"); // or specify your domain instead of '*'
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
