24aab28b0f4512470370e88192db3031e151337b
pao <paolo.jon@digile.com>
2026-06-18 15:54:51 +0800

    update on gethierarchy endpoint

--- models/AssetModel.php
+++ models/AssetModel.php
@@ -278,16 +278,16 @@ class AssetModel
                 "message" => "Table '$table' does not exist."
             ];
         }
-        //setup hierarchy - display all records - parent_id is null or empty
+
         $sql = "
             SELECT *
             FROM `$table`
-            WHERE 
-            c_import_batch = '$importBatch'
-            AND
-            c_parent_id IS NULL
-            OR 
-            TRIM(c_parent_id) = ''
+            WHERE
+                c_import_batch = '$importBatch'
+                AND (
+                    c_parent_id IS NULL
+                    OR TRIM(c_parent_id) = ''
+                )
             ORDER BY id DESC
         ";
 
@@ -301,6 +301,7 @@ class AssetModel
 
         return [
             "status" => "success",
+            "total_count" => $result->num_rows,
             "assets" => $assets
         ];
     }
